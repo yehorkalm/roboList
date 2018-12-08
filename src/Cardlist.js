32af407ae7a82d21cd602169f1c205f90d89a9cb
yehorkalm <swabston@gmail.com>
2018-12-07 22:06:31 -0500

    upload

---
+++ src/Cardlist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './Card';
+
+const Cardlist = ({ robots }) => {
+  const cardArray = robots.map((robot, i) => {
+    return (
+      <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+    );
+  });
+
+  return <div>{cardArray}</div>;
+};
+
+export default Cardlist;
