1e9fb42c9e130a86d4b8b37bad22c7b583aac481
yehorkalm <swabston@gmail.com>
2018-12-07 22:11:19 -0500

    touchups

--- src/Cardlist.js
+++ src/Cardlist.js
@@ -2,13 +2,20 @@ import React from 'react';
 import Card from './Card';
 
 const Cardlist = ({ robots }) => {
-  const cardArray = robots.map((robot, i) => {
-    return (
-      <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
-    );
-  });
-
-  return <div>{cardArray}</div>;
+  return (
+    <div>
+      {robots.map((robot, i) => {
+        return (
+          <Card
+            key={i}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Cardlist;
